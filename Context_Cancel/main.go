83f661c59f6c09496445ac496d2917ca13a393df
akaAminZhi <min3832170@163.com>
2022-03-25 17:56:06 +0800

    test

--- Context_Cancel/main.go
+++ Context_Cancel/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func doSomething(ctx context.Context) {
 
 	select {
+	case <-time.After(3 * time.Second):
+		fmt.Println("Finish do something")
 	case <-ctx.Done():
-		fmt.Println("context cancel")
+		err := ctx.Err()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println("context cancel yo")
 	}
 }
 
@@ -18,7 +25,12 @@ func main() {
 	ctx := context.Background()
 	//接着使用cancel
 	ctx, cancel := context.WithCancel(ctx)
+	// 创建一个进程去执行 cancel，2秒后
+	go func() {
+		time.Sleep(2 * time.Second)
+		cancel()
+	}()
 
-	cancel()
+	doSomething(ctx)
 
 }
